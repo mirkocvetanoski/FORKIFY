8b9128aaa49b29eaa29167ccd657c94633140d33
dreadpirate2022 <mirko.cvetanoski@gmail.com>
2023-11-01 12:55:53 +0100

    New API Key

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'df363b9a-dda8-4f7a-8b13-99a03da06e81';
+export const MODAL_CLOSE_SEC = 2.5;
